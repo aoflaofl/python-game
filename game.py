47327c24a0ad32c11355175948006f2fe45dbbd3
Gene Johannsen <aofl@mail.com>
2025-11-14 17:28:50 -0800

    Implement alien character with animations and sound effects

--- game.py
+++ game.py
@@ -9,17 +9,38 @@ TITLE = "Python Game Stub"
 
 message = "Hello from Pygame Zero!"
 
+alien = Actor('alien')
+alien.pos = 100, 56
 
 def draw():
     screen.clear()
-    screen.fill((30, 30, 30))
+    screen.fill((128, 30, 30))
     screen.draw.text(
         message,
         center=(WIDTH // 2, HEIGHT // 2),
         fontsize=48,
         color="white",
     )
+    alien.draw()
 
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.eep.play()
+    clock.schedule_unique(set_alien_normal, 1.0)
+
+def set_alien_normal():
+    alien.image = 'alien'
 
 if __name__ == "__main__":
     pgzrun.go()
+
